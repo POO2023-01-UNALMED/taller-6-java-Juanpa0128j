bbd4ed29bb6a7538383d804d36255c1ddc0c04bb
JuanPMejia <66537159+Juanpa0128j@users.noreply.github.com>
2023-03-27 16:02:02 -0500

    Add files via upload

---
+++ Camioneta.java
@@ -0,0 +1,26 @@
+package vehiculos;
+
+public class Camioneta extends Vehiculo{
+	private Boolean volco;
+	private static int cantidadCamionetas;
+	
+	public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, Boolean volco) {
+		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
+		this.volco = volco;
+		cantidadCamionetas++;
+		
+	}
+	
+	public static int getCantidadCamionetas() {
+		return cantidadCamionetas;
+	}
+	
+	public Boolean isVolco() {
+		return volco;
+	}
+	
+	public void setVolco(Boolean volco) {
+		this.volco = volco;
+	}
+	
+}
